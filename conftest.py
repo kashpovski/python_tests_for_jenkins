878c8ad9becfe041cf7ebcfd839f3a314904dff2
kashpovski <107947931+kashpovski@users.noreply.github.com>
2022-10-07 19:01:07 +0300

    add Selenoid

--- conftest.py
+++ conftest.py
@@ -13,19 +13,26 @@ def pytest_addoption(parser):
                      default="chrome",
                      choices=["chrome", "firefox", "MicrosoftEdge", "opera", "yandex"],
                      help="Browser to run tests")
-    parser.addoption("--remote", action="store_true", help="Open browser in remote")
-    parser.addoption("--headless", action="store_true", help="Browser in headless mode")
-    parser.addoption("--driver", default=r"D:\Mars\QA\tools\WebDrivers", help="Directory to the webdriver")
+    parser.addoption("--driver_path", default=r"D:\Mars\QA\tools\WebDrivers", help="Directory to the webdriver")
     parser.addoption("--url", default="http://192.168.31.185:8081/", help="Base url")
-    parser.addoption("--executor", default="192.168.147.1", help="Remote ip address")
+    parser.addoption("--headless", action="store_true", help="Browser in headless mode")
     parser.addoption("--fullscreen", action="store_true", help="Open browser in full-screen mode")
     parser.addoption("--log_level", default="DEBUG", help="Set log level")
     parser.addoption("--log_proxy", action="store_true", help="Open browser in log proxy")
     parser.addoption("--log_browser", action="store_true", help="Open browser in log browser")
-    parser.addoption("--platform_name",
-                     default="Windows 10",
-                     choices=["Windows XP", "Windows 10"],
-                     help="Platform name for remote ran")
+    parser.addoption("--remote",
+                     default="local",
+                     choices=["selenium", "selenoid"],
+                     help="Open browser in remote (selenium or selenoid)")
+    parser.addoption("--remote_executor", default="192.168.31.185", help="Remote ip address")
+    parser.addoption("--remote_platform_name", help="Selenium: Platform name for remote ran")
+    parser.addoption("--remote_bv", help="Selenoid: change browser version (ex. 104.0)")
+    parser.addoption("--remote_vnc", action="store_true", help="Selenoid: switch vnc")
+    parser.addoption("--remote_video", action="store_true", help="Selenoid: switch recording video")
+    parser.addoption("--remote_log", action="store_true", help="Selenoid: switch log")
+    parser.addoption("--remote_name", help="Selenoid: name user")
+    parser.addoption("--remote_sr", help="Selenoid: change screen resolution (ex. 1280x1024)")
+
 
 
 @pytest.fixture
@@ -50,15 +57,22 @@ def proxy_server(request):
 def browser(request, proxy_server):
     browser_name = request.config.getoption("--browser")
     headless = request.config.getoption("--headless")
-    driver = request.config.getoption("--driver")
+    driver_path = request.config.getoption("--driver_path")
     url = request.config.getoption("--url")
     fullscreen = request.config.getoption("--fullscreen")
     log_level = request.config.getoption("--log_level")
     log_proxy = request.config.getoption("--log_proxy")
     log_browser = request.config.getoption("--log_browser")
     remote = request.config.getoption("--remote")
-    executor = request.config.getoption("--executor")
-    platform_name = request.config.getoption("--platform_name")
+    remote_executor = request.config.getoption("--remote_executor")
+    remote_platform_name = request.config.getoption("--remote_platform_name")
+    remote_bv = request.config.getoption("--remote_bv")
+    remote_vnc = request.config.getoption("--remote_vnc")
+    remote_video = request.config.getoption("--remote_video")
+    remote_log = request.config.getoption("--remote_log")
+    remote_name = request.config.getoption("--remote_name")
+    remote_sr = request.config.getoption("--remote_sr")
+
 
     logger = logging.getLogger(request.node.name)
     file_handler = logging.FileHandler(f"logs/logs_tests/{request.module.__name__}-{request.function.__name__}.log")
@@ -68,10 +82,11 @@ def browser(request, proxy_server):
     logger.setLevel(level=log_level)
 
     start_time = datetime.datetime.now()
-    logger.info(f"===> Test started ===>")
+    logger.info("======== Test started ========")
 
     caps = DesiredCapabilities.CHROME
     options = webdriver.ChromeOptions()
+
     if headless:
         options.headless = True
         logger.info("headless mode")
@@ -86,28 +101,50 @@ def browser(request, proxy_server):
         }
         logger.info("log browser on")
 
-    if remote:
-        _browser = webdriver.Remote(command_executor=f"{executor}:4444/wd/hub",
-                                    desired_capabilities={"browserName": browser_name,
-                                                          "platformName": platform_name
-                                                          },
-                                    options=options)
-        logger.info(f"remote mode (Selenium server: {executor}, Platform name: {platform_name})")
-    elif browser_name == "chrome":
-        _browser = webdriver.Chrome(executable_path=driver + "\chromedriver",
-                                    options=options,
-                                    desired_capabilities=caps)
-    elif browser_name == "firefox":
-        _browser = webdriver.Firefox(executable_path=driver + "\geckodriver")
-    elif browser_name == "opera":
-        _browser = webdriver.Opera(executable_path=driver + "\operadriver")
-    elif browser_name == "yandex":
-        _browser = webdriver.Chrome(executable_path=driver + "\yandexdriver",
-                                    options=options)
-    elif browser_name == "MicrosoftEdge":
-        _browser = webdriver.Edge(executable_path=driver + "\msedgedriver")
+    if remote == "local":
+        if browser_name == "chrome":
+            _browser = webdriver.Chrome(executable_path=driver_path + "\chromedriver",
+                                        options=options,
+                                        desired_capabilities=caps)
+        elif browser_name == "firefox":
+            _browser = webdriver.Firefox(executable_path=driver_path + "\geckodriver")
+        elif browser_name == "opera":
+            _browser = webdriver.Opera(executable_path=driver_path + "\operadriver")
+        elif browser_name == "yandex":
+            _browser = webdriver.Chrome(executable_path=driver_path + "\yandexdriver",
+                                        options=options)
+        elif browser_name == "MicrosoftEdge":
+            _browser = webdriver.Edge(executable_path=driver_path + "\msedgedriver")
+        else:
+            raise ValueError(f"Browser '{browser_name}' is not supported ")
     else:
-        raise ValueError(f"Browser '{browser_name}' is not supported ")
+        capabilities = {}
+        if remote == "selenium":
+            capabilities = {
+                "browserName": browser_name,
+                "platformName": remote_platform_name
+            }
+            logger.info(f"remote mode (Selenium server: {remote_executor}, Platform name: {remote_platform_name})")
+        elif remote == "selenoid":
+            capabilities = {
+                "browserName": browser_name,
+                "browserVersion": remote_bv,
+                "selenoid:options": {
+                    "screenResolution": remote_sr,
+                    "name": remote_name,
+                    "enableVNC": remote_vnc,
+                    "enableVideo": remote_video,
+                    "enableLog": remote_log,
+                },
+                "acceptSslCerts": True,
+                "acceptInsecureCerts": True,
+                # "timeZone": 'Europe/Moscow',
+                # "goog:chromeOptions": {"mobileEmulation": {"deviceName": "iPhone 5/SE"}}
+            }
+            logger.info(f"remote mode (Selenoid {remote_executor})")
+        _browser = webdriver.Remote(command_executor=f"{remote_executor}:4444/wd/hub",
+                                    desired_capabilities=capabilities,
+                                    options=options)
 
     if fullscreen:
         _browser.maximize_window()
@@ -160,7 +197,7 @@ def browser(request, proxy_server):
                                log_proxy)
         # _browser.proxy.close()
         _browser.quit()
-        logger.info(f"<=== Test finished. {datetime.datetime.now() - start_time} <===")
+        logger.info(f"^^^^^^^^ Test finished. {datetime.datetime.now() - start_time} ^^^")
 
     request.addfinalizer(fin)
 
